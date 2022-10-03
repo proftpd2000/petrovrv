bc6ad882ce941a2d071311431d31fab019874e5d
Roman <1423721@mail.ru>
2022-10-03 19:24:33 +0300

    catalog.html editor

---
+++ img/index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+ <head>
+ <!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(87377967, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/87377967" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+рами работ по ремонту промышленной, бытовой и автомобилной электроники.">
+  <meta name="keywords" content="разработка электроники 
+  <meta charset="UTF-8">
+  <meta name="description" content="Первая страница с примерами работ по ремонту промышленной, бытовой и автомобилной электроники.">
+  <meta name="keywords" content="разработка электроники на заказ, разработка электроники на заказ москва	 , инженер эмс, инженер радиотехники, инженер схемотехник, схемотехник, разработка схемы, разработка схемы устройства, разработка печатных плат, разработка печатных плат на заказ, разработка электрических схем, разработка электрических схем на заказ,разработка электроники на заказ, проверка работоспособности радиостанции,проверка работы радиостанции, проверка антенны, ремонт автомобильных радиостанций, ремонт радиостанций в москве, программирование микросхем, мастер по ремонту компьютеров на дому, ремонт компьютеров частник, Петров Роман ">
+  <meta name="author" content="roman" >
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Первая страница с примерами работ</title>
+    <link href="img/favicon.ico" title="Петров Роман" type="image/vnd.microsoft.icon" rel="icon">
+    <link rel="stylesheet" href="fontawesome/css/all.min.css"> <!-- https://fontawesome.com/ -->
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet">
+    <!-- https://fonts.google.com/ -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/templatemo-video-catalog.css">
+
+</head>
+
+<body>
+<!-- top -->
+<?php require 'top'; ?>
+                    
+<div class="row tm-catalog-item-list">                   
+
+ <div class="col-lg-4 col-md-6 col-sm-12 tm-catalog-item">  
+ <div class="position-relative tm-thumbnail-container">
+<a href="https://www.youtube.com/embed/CTd6S-2PnHk" title="Ремонт электронного блока коробки передач автомобиля Volvo">
+<img src="img/index_1/index_1.png" width="400" height="200" alt="Ремонт электроннрго блока коробки передач">
+</a>
+ </div>
+<div class="p-4 tm-bg-gray tm-catalog-item-description">
+<h3 class="tm-text-primary mb-3 tm-catalog-item-title">Ремонт электроники коробки передач Volvo</h3>
+<p class="tm-catalog-item-text">
+Ремонт электронного блока коробки передач автомобиля Volvo. Затратил на поиск неисправности 7 часов, заработал 120$       
+</p>
+</div>
+</div>  
+                        
+<div class="col-lg-4 col-md-6 col-sm-12 tm-catalog-item"> 
+ <div class="position-relative tm-thumbnail-container">
+ <a href="https://www.youtube.com/embed/2nONeDrMjPA" title="Ремонт AirBag">
+<img src="img/index_1/index_2.png" width="400" height="200" alt="Ремонт AirBag">
+</a>
+
+ </div>
+<div class="p-4 tm-bg-gray tm-catalog-item-description">
+<h3 class="tm-text-primary mb-3 tm-catalog-item-title">Ремонт AirBag</h3>
+<p class="tm-catalog-item-text">
+Доработка диагностического кабеля  Delphi DS150 для возможности диагностики отдельно автомбильных блоков.
+</p>
+</div>
+</div> 
+
+        
+                        
+ <div class="col-lg-4 col-md-6 col-sm-12 tm-catalog-item">  
+ <div class="position-relative tm-thumbnail-container">
+<a href="https://www.youtube.com/embed/8e8ekyXZNOI" title="Ремонт блока EBS Knorr-Bremse Renault">
+<img src="img/index_1/index_3.png" width="400" height="200" alt="Ремонт Ремонт блока EBS Knorr-Bremse Renault">
+</a>
+ 
+                              </div>
+                            <div class="p-4 tm-bg-gray tm-catalog-item-description">
+                                <h3 class="tm-text-primary mb-3 tm-catalog-item-title">Ремонт блока EBS Knorr-Bremse Renault</h3>
+                                <p class="tm-catalog-item-text">
+                              Примент ремонта блока EBS Knorr-Bremse Renault. Затратил 30 часов. Пришлось разбираться в схемотехнике и ее прорисовывать, чтобы понять принцип работы.     
+                                </p>
+                            </div>
+                        </div>                     
+                    
+ <div class="col-lg-4 col-md-6 col-sm-12 tm-catalog-item">
+<div class="position-relative tm-thumbnail-container">
+<a href="https://www.youtube.com/embed/di3SEIhIQGg" title="Терморегулятор">
+<img src="img/index_1/index_4.png" width="400" height="200" alt="Разработка регулятора температуры для паяльника">
+</a>
+</div>    
+                            <div class="p-4 tm-bg-gray tm-catalog-item-description">
+                                <h3 class="tm-text-primary mb-3 tm-catalog-item-title">Терморегулятор</h3>
+                                <p class="tm-catalog-item-text"><span class="tm-text-secondary"></span>  
+                                    Программируемый регулятор температуры для паяльника 12В на микроконтроллере AVR.
+                                    Максимальный ток коммутации 50А, имеет защиту от смены полярности напряжения.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-12 tm-catalog-item">
+                            <div class="position-relative tm-thumbnail-container">
+<a href="https://www.youtube.com/embed/3mTHtCPG5ZM" title="Усилитель 50 Вт">
+<img src="img/index_1/index_5.png" width="400" height="200" alt="Усилитель 50 Вт">
+</a>                            
+                         </div>
+                            <div class="p-4 tm-bg-gray tm-catalog-item-description">
+                                <h3 class="tm-text-primary mb-3 tm-catalog-item-title">Усилитель 2*25Вт.</h3>
+                                <p class="tm-catalog-item-text">
+                Усилитель мощности с верхней граничной частотой 460 кГц  -3Дб  
+                Может работать в режиме моста, имеет  
+               защиту от смены полярности напряжения питания, защиту от высокого и низкого уровня напряжения.             
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-12 tm-catalog-item">
+                            <div class="position-relative tm-thumbnail-container">
+                            
+<a href="https://www.youtube.com/embed/UGTpTb_3XbE" title="Активная колонка">
+<img src="img/index_1/index_6.png" width="400" height="200" alt="Активная колонка">
+</a>   
+                            </div>                            
+                            <div class="p-4 tm-bg-gray tm-catalog-item-description">
+                                <h3 class="tm-text-primary mb-3 tm-catalog-item-title">Активная колонка</h3>
+                                <p class="tm-catalog-item-text">Активная колонка с разделение полос, с возможностью переключения в пассивный режим и подключения к выходу любого усилителя мощности.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-12 tm-catalog-item">
+                            <div class="position-relative tm-thumbnail-container">
+                            
+<a href="https://www.youtube.com/embed/Dyw6RkrZKiA" title="Установка ССД-диска">
+<img src="img/index_1/index_7.png" width="400" height="200" alt="Установка ССД-диска">
+</a>                               
+                            </div>    
+                            <div class="p-4 tm-bg-gray tm-catalog-item-description">
+                                <h3 class="tm-text-primary mb-3 tm-catalog-item-title">Установка SSD-диска</h3>
+                                <p class="tm-catalog-item-text">Установка SSD-диска в ноутбук, где он не предусмотрен.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-12 tm-catalog-item">
+                            <div class="position-relative tm-thumbnail-container">
+ <a href="https://www.youtube.com/embed/WtJEU8HhMq0" title="Разработка фильтра узкополосного">
+<img src="img/index_1/index_8.png" width="400" height="200" alt="Разработка фильтра узкополосного">
+</a></div> 
+                            <div class="p-4 tm-bg-gray tm-catalog-item-description">
+                                <h3 class="tm-text-primary mb-3 tm-catalog-item-title">Фильтр ПЧ 10.7 МГц</h3>
+                                <p class="tm-catalog-item-text">Разработка двухполосного фильтра промежуточной частоты 10.7 МГц для СВ-радиостанции.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-12 tm-catalog-item">
+                            <div class="position-relative tm-thumbnail-container">
+ <a href="https://www.youtube.com/embed/rxaEudbryGc" title="Блок Питания с низким уровнем помех">
+<img src="img/index_1/index_9.png" width="400" height="200" alt="Блок Питания с низким уровнем помех">
+</a>                            
+                                                          </div>                            
+                            <div class="p-4 tm-bg-gray tm-catalog-item-description">
+                                <h3 class="tm-text-primary mb-3 tm-catalog-item-title">Блок питания</h3>
+                                <p class="tm-catalog-item-text">Разарботка блока питания с низким уровнем помех.
+                               Блок питания получил регулируемую защиту по току.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-12 tm-catalog-item">
+                            <div class="position-relative tm-thumbnail-container">
+ <a href="https://www.youtube.com/embed/BrFgcDHPLHw" title="Испытание ИБП на ЭМС">
+<img src="img/index_1/index_10.png" width="400" height="200" alt="Испытание ИБП на ЭМС">
+</a>                               
+</div>    
+                            <div class="p-4 tm-bg-gray tm-catalog-item-description">
+                                <h3 class="tm-text-primary mb-3 tm-catalog-item-title">Испытание ИБП на ЭМС</h3>
+                                <p class="tm-catalog-item-text">Испытание импульсного блока питания китайского производства на уровень помех создаваемый в цепях питания.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-12 tm-catalog-item">
+                            <div class="position-relative tm-thumbnail-container">
+ <a href="https://www.youtube.com/embed/wn_k7tN1bNk" title="Автоинформатор">
+<img src="img/index_1/index_11.png" width="400" height="200" alt="Автоинформатор">
+</a>                                 
+</div>
+                            <div class="p-4 tm-bg-gray tm-catalog-item-description">
+                                <h3 class="tm-text-primary mb-3 tm-catalog-item-title">Автоинформатор</h3>
+                                <p class="tm-catalog-item-text">Пример разработки автоинформатора на микрокнтроллере AVR для радиостанции. Автоинформатор управляет передачей радиостанции и  позволяет воспроизводить множество аудиотреков с программируемым интервалом времени. Схема выполнена с учетом ЭМС.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-12 tm-catalog-item">
+ 
+<div class="position-relative tm-thumbnail-container">
+ <a href="https://www.youtube.com/embed/jSGKuJ8g2kM" title="Автосигнализация 430 МГц">
+<img src="img/index_1/index_12.png" width="400" height="200" alt="Автосигнализация 430 МГц">
+</a>     
+ </div>
+                                                  
+                            <div class="p-4 tm-bg-gray tm-catalog-item-description">
+                                <h3 class="tm-text-primary mb-3 tm-catalog-item-title">Автосигнализация 430 МГц</h3>
+                                <p class="tm-catalog-item-text">Пример разработки автосигнализации на 430 МГц. Сигнализация позволяет подключать несколько датчико Доплера и  управлять топливным насосм иммитируя неисправность топливной системы.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Catalog Paging Buttons -->
+                   
+                </main>
+<?php require 'page'; ?>
+                <!-- Subscribe form and footer links -->
+                <div class="row mt-5 pt-3">
+                    <div class="col-xl-6 col-lg-12 mb-4">
+                        <div class="tm-bg-gray p-5 h-100">
+                            <h3 class="tm-text-primary mb-3">ИНФОРМАЦИЯ </h3>
+
+<hr><p style="font-size: 8 ">
+Частоты для связи: <b>27, 135 МГц</b> и <b>434.625 МГц CTCSS 186,2 Гц </b> <br>
+Используемые антенны: "Антенна Надененко" и "Антенна 5/8 "<br>                       
+ </p>
+          </div>                
+                      
+                    </div>
+                  
+<!-- bottom -->
+<?php require 'bottom'; ?>
+
+</body>
+
+</html>
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(86259011, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/86259011" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
